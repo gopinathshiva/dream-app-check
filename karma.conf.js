4525433d80e0395a4e65786c047ba37679afd5e3
gosivasa <gopinath.sivasamy@appdynamics.com>
2020-03-19 11:29:29 +0530

    fix

--- karma.conf.js
+++ karma.conf.js
@@ -11,6 +11,7 @@ module.exports = function (config) {
       require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-teamcity-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
