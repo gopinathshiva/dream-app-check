e889326edbee6500fe23aa38f4df53f38e932b21
gosivasa <gopinath.sivasamy@appdynamics.com>
2020-03-19 14:55:44 +0530

    fail test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'my-dream-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-dream-app');
+    expect(app.title).toEqual('my-dream-appsss');
   });
 
   it('should render title', () => {
