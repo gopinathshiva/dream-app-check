45905afbec7256a9082b1af94937bd680e09bade
gosivasa <gopinath.sivasamy@appdynamics.com>
2020-03-20 16:29:26 +0530

    test fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'my-dream-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-dream-appssss');
+    expect(app.title).toEqual('my-dream-app');
   });
 
   it('should render title', () => {
