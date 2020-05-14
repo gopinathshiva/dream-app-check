3023058380f191460d863a86d6e4b46a4e1b0b7a
gosivasa <gopinath.sivasamy@appdynamics.com>
2020-05-14 16:11:12 +0530

    revert

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -39,7 +39,7 @@ project {
 
     val buildChain = sequential {
       buildType(Build)
-      parallel(options = { onDependencyFailure = FailureAction.CANCEL }) {
+      parallel {
         buildType(FirefoxTest)
         buildType(ChromeTest)
       }
@@ -143,6 +143,8 @@ object Build : BuildType({
         root(HttpsGithubComGopinathshivaDreamAppCheckRefsHeadsMaster)
     }
 
+//    name = "${DslContext.getParameter("name", "Default name")}
+
     steps{
 //      script {
 //        name = "Install"
@@ -158,7 +160,7 @@ object Build : BuildType({
 //      }
         script{
           name = "echo"
-          scriptContent = "Exit 1"
+          scriptContent = "Build successful"
         }
     }
 
